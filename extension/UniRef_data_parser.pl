6f59352a4212a1b87ea0ad6c9cd46ec7742205a4
J. Allen Baron <jabaron.phd@gmail.com>
2020-07-15 14:49:41 -0700

    Remove headers from UniRef90 parsed data file

--- extension/UniRef_data_parser.pl
+++ extension/UniRef_data_parser.pl
@@ -26,7 +26,6 @@ main: {
     }
     
     open ($ofh, ">$outfile") or die "Error, cannot write to $outfile";
-    print $ofh join("\t", "Accession", "LinkId", "AttributeType") . "\n";
     
     my $id;
     my $descr;
